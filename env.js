51ebaad2023194d08a68540dc208d37b7626e03e
BrianWu <brianwu291@gmail.com>
2022-03-13 22:24:37 +0800

    setup db config

--- env.js
+++ env.js
@@ -5,29 +5,29 @@ config();
 const env = {
   production: {
     database: {
-      name: process.env.productionDBName,
-      host: process.env.productionDbHost,
-      user: process.env.productionDbUser,
-      password: process.env.productionDbPassword,
-      port: process.env.productionDbPort,
+      host: process.env.RDS_HOSTNAME,
+      user: process.env.RDS_USERNAME,
+      password: process.env.RDS_PASSWORD,
+      port: process.env.RDS_PORT,
+      name: process.env.RDS_DB_NAME,
     },
   },
   development: {
     database: {
-      name: process.env.developmentDBName,
       host: process.env.developmentDbHost,
       user: process.env.developmentDbUser,
       password: process.env.developmentDbPassword,
       port: process.env.developmentDbPort,
+      name: process.env.developmentDBName,
     },
   },
   test: {
     database: {
-      name: process.env.testDBName,
-      host: process.env.testDbHost,
-      user: process.env.testDbUser,
-      password: process.env.testDbPassword,
-      port: process.env.testDbPort,
+      host: process.env.RDS_HOSTNAME,
+      user: process.env.RDS_USERNAME,
+      password: process.env.RDS_PASSWORD,
+      port: process.env.RDS_PORT,
+      name: process.env.RDS_DB_NAME,
     },
   },
 };
